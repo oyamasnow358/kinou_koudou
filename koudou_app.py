a127f1ac5ca992cff54b2499b79429af15d4f28b
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-02-09 15:22:39 +0900

    AS

--- koudou_app.py
+++ koudou_app.py
@@ -10,8 +10,8 @@ import matplotlib.font_manager as fm  # 日本語フォント設定に必要
 font_path = r"C:\Users\taka\OneDrive\デスクトップ\アプリ開発\機能的行動評価\kinou_koudou\ipag.ttf\ipaexg.ttf"
   # 実際のパスに置き換えます
 
-
 # フォントファイルが存在するか確認して設定
+font_prop = None  # 初期化
 if os.path.exists(font_path):
     font_prop = fm.FontProperties(fname=font_path)  # フォントプロパティを作成
     matplotlib.rc('font', family=font_prop.get_name())  # 日本語フォントを適用
