0a57f7167258be51089491c67c8b2e45768a6091
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-02-09 14:10:11 +0900

    AS

--- koudou_app.py
+++ koudou_app.py
@@ -1,5 +1,12 @@
+import matplotlib
 import matplotlib.font_manager as fm
+import os
 
-# システムにインストールされているフォント一覧を取得
-for font in fm.findSystemFonts(fontpaths=None, fontext='ttf'):
-    print(font)
+# フォントパス設定
+font_path = "./ipag.ttf"
+if os.path.exists(font_path):
+    font_prop = fm.FontProperties(fname=font_path)
+    matplotlib.rc('font', family=font_prop.get_name())
+    print("日本語フォントが設定されました！")
+else:
+    print("フォントファイルが見つかりません。")
